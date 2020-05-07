1de6bf6af92bb581b98a77a16e62414d6853a8cc
lifeoflikhon <mehedi.hasansjs@gmail.com>
2020-05-07 17:58:23 +0600

    01_swap

---
+++ python/01_swap_without_temp.py
@@ -0,0 +1,16 @@
+# ⓒ DIU CPC
+# Logic Series
+
+# PROBLEM:
+# The program takes both the values from the user
+# and swaps them without using temporary variable
+
+value1 = int(input("Enter value of first variable: "))
+value2 = int(input("Enter value of second variable: "))
+
+# swaping
+value1 = value1 + value2
+value2 = value1 - value2
+value1 = value1 - value2
+
+# Happy Coding ✌🏻
